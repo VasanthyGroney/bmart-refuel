9e29c1d8691c20e4e8b98b83069184297275b4e0
Теодор Тодоров <teo.plamka@gmail.com>
2024-05-07 15:22:35 +0200

    Change the code in PyCharm

--- plam.py
+++ plam.py
@@ -1,47 +1,48 @@
 import json
-from math import radians, sin, cos, sqrt, atan2
 
-def load_fuel_stations(filename):
+def get_phone_number():
+    return input("Enter your phone number: ")
+
+def load_database(filename):
     with open(filename, 'r') as file:
         return json.load(file)
 
+def check_database(database, phone):
+    if phone in database:
+        found = True
+        data = database[phone]
+    else:
+        found = False
+        data = None
+    return found, data
 
-def calculate_distance(lat1, lon1, lat2, lon2):
-    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+def save_user_params(user_params, filename, phone_number, database):
+    database[phone_number] = user_params
+    with open(filename, 'w') as file:
+        return json.load(database)
 
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    distance = 6371 * c
+def get_user_params():
+    location = input("Enter your address: ")
+    radius = input("Enter a radius: ")
+    fuel = input("What do you want to fuel: ")
+    return location, radius, fuel
 
-    return distance
-
-
-def search_fuel_stations(fuel_stations, location, radius):
-    location_lat, location_lon = map(float, location.split(','))
-    results = []
-    for station in fuel_stations:
-        station_lat, station_lon = station['coordinates']['latitude'], station['coordinates']['longitude']
-        distance = calculate_distance(location_lat, location_lon, station_lat, station_lon)
-        if distance <= radius:
-            results.append(station)
-    return results
-
-
-def search_nearby_fuel_stations():
-    fuel_stations = load_fuel_stations('sms_project.json')
-
-    phone = input("Enter your phone number: ")
-    location = input("Enter your location: ")
-    fuel_type = input("Enter the type of fuel (gasoline/diesel/electric): ")
-    radius = int(input("Enter the search radius (in km): "))
-
-    search_results = search_fuel_stations(fuel_stations, location, radius)
-    print("Search Results:")
-    for result in search_results:
-        print(result)
+def get_all_inputs():
+    phone_number = get_phone_number()  # gets phone number
+    database = load_database("sms_project.json")
+    found, data = check_database(phone_number, database)  # gets info if phonenumber is in database and gets data
+    if found == True:
+        user_params = {}
+        for key, val in data.items():
+            print(f"{key}: {val}")
+            answer_choice = input(f"Do you want to use the {key}?")
+            if "n" in answer_choice.lower():
+                val = input(f"Enter a {key}: ")
+            user_params[key] = val
+        save_user_params(user_params, "sms_project.json")
+    get_user_params()
+    save_user_params()
 
 
 if __name__ == "__main__":
-    search_nearby_fuel_stations()
+     get_all_inputs()
