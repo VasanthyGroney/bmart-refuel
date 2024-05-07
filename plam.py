27f92d655ba6f2ab21cb946951a2cde65fc938e9
Теодор Тодоров <teo.plamka@gmail.com>
2024-05-07 11:47:09 +0200

    plam.py

--- plam.py
+++ plam.py
@@ -1,11 +1,9 @@
 import json
+from math import radians, sin, cos, sqrt, atan2
 
 def load_fuel_stations(filename):
     with open(filename, 'r') as file:
         return json.load(file)
-
-
-from math import radians, sin, cos, sqrt, atan2
 
 
 def calculate_distance(lat1, lon1, lat2, lon2):
@@ -20,7 +18,7 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     return distance
 
 
-def search_fuel_stations(fuel_stations, phone, location, fuel_type, radius):
+def search_fuel_stations(fuel_stations, location, radius):
     location_lat, location_lon = map(float, location.split(','))
     results = []
     for station in fuel_stations:
@@ -31,19 +29,19 @@ def search_fuel_stations(fuel_stations, phone, location, fuel_type, radius):
     return results
 
 
-def main():
-    fuel_stations = load_fuel_stations('fuel_stations.json')
+def search_nearby_fuel_stations():
+    fuel_stations = load_fuel_stations('sms_project.json')
 
     phone = input("Enter your phone number: ")
     location = input("Enter your location: ")
     fuel_type = input("Enter the type of fuel (gasoline/diesel/electric): ")
     radius = int(input("Enter the search radius (in km): "))
 
-    search_results = search_fuel_stations(fuel_stations, phone, location, fuel_type, radius)
+    search_results = search_fuel_stations(fuel_stations, location, radius)
     print("Search Results:")
     for result in search_results:
         print(result)
 
 
 if __name__ == "__main__":
-    main()
+    search_nearby_fuel_stations()
