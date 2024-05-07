9c14b7cc139f95d6277cfed57b00120d719aacbe
Теодор Тодоров <teo.plamka@gmail.com>
2024-05-07 19:47:43 +0200

    change the py. code

--- plam.py
+++ plam.py
@@ -28,22 +28,30 @@ def save_user_params(user_params, filename, phone_number, database):
 
 def get_user_params():
     location = input("Enter your address: ")
-    radius = input("Enter a radius: ")
-    fuel = input("What do you want to fuel: ")
+    while True:
+        radius = input("Enter a radius in km (max. 25): ")
+        if 0 < int(radius) <= 25:
+            break
+        print("You have entered a wrong radius.\nTry again.")
+    while True:
+        fuel = input("What do you want to fuel (Enter 'e5', 'e10' or 'diesel': ")
+        if fuel in ['e5', 'e10', 'diesel']:
+            break
+        print("Wrong fuel type.\nTry again")
     return location, radius, fuel
 
 
 def get_all_inputs():
     phone_number = get_phone_number()  # gets phone number
     database = load_database("sms_project.json")
-    found, data = check_database(phone_number, database)  # gets info if phonenumber is in database and gets data
+    found, data = check_database(phone_number, database)
     if found:
         user_params = {}
         for key, val in data.items():
             print(f"{key}: {val}")
-            answer_choice = input(f"Do you want to use the {key}?")
+            answer_choice = input(f"Do you want to use the same {key}?")
             if "n" in answer_choice.lower():
-                val = input(f"Enter a {key}: ")
+                val = input(f"Enter a new {key}: ")
             user_params[key] = val
         save_user_params(user_params, "sms_project.json", phone_number, database)
         return phone_number, user_params
