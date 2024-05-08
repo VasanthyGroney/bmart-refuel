9f7eaf1a3a99cc9f60b047430f356ca7930a6730
Vasanthy Groney <vasantymarshal@gmail.com>
2024-05-08 10:09:34 +0200

    with new Api

--- vasanthy.py
+++ vasanthy.py
@@ -2,18 +2,15 @@ import requests
 import json
 
 
-BEGIN_URL = "https://nominatim.openstreetmap.org/search?addressdetails=1&q="
-END_URL = "&format=jsonv2&limit=1"
+COORD_BASE_URL = "https://geocode.maps.co/search?q="
+API_KEY = "663b3080937e8329575847yan7279cb"
 SMS_BASE_URL = 'https://43l848.api.infobip.com/sms/2/text/advanced'
 AUTHORIZATION = 'App 3dec643d54c66b6fab8fbfcce32f8081-27aba670-39dd-40be-8371-0f5acd088797'
 
 
 def get_coordinates(user_address):
     address = user_address.replace(" ", "+")
-    while True:
-        res = requests.get(BEGIN_URL + address + END_URL)
-        if res != None:
-            break
+    res = requests.get(COORD_BASE_URL + address + "&api_key=" + API_KEY)
     data = json.loads(res.text)
     lon = data[0].get("lon")
     lat = data[0].get("lat")
